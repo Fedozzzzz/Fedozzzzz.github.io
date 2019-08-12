de75968857842c7662bce614a967bc272156e426
FedozZz <titov.bigf@gmail.com>
2019-08-12 19:38:26 +0300

    no message

---
+++ precache-manifest.4266689b62569f0bbbefe700c52fdc5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e6826bc3570043c337acd84c22747a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b386b22914af99373689",
+    "url": "/static/css/main.3d6679ac.chunk.css"
+  },
+  {
+    "revision": "1f1e70a61d94405a999b",
+    "url": "/static/js/2.158f2d9c.chunk.js"
+  },
+  {
+    "revision": "b386b22914af99373689",
+    "url": "/static/js/main.7a478fc1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "792189d69d32520466a68bc6a95bebcb",
+    "url": "/static/media/black.792189d6.svg"
+  }
+]);
